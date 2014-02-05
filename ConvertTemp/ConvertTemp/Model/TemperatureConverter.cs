7ce436ad3a59a89e85dd9b8adb416c0d3963ae86
hg222dt <hg222dt@student.lnu.se>
2014-02-05 14:35:10 +0100

    Har skapat klass som gör temperaturberäkningar, samt skrivit kod som anropar de statiska metoderna i denna klass för att kunna skriva ut efterfrågade temperaturer.

---
+++ ConvertTemp/ConvertTemp/Model/TemperatureConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConvertTemp.Model
+{
+    public static class TemperatureConverter
+    {
+        public static void CelsiusToFahrenheit(int degreesC)
+        {
+            double degreesF = degreesC * 1.8 + 32;
+
+        }
+
+        public static void FahrenheitToCelcius(int degreesF)
+        {
+            double degreesC = (degreesF - 32) / 1.8;
+
+        }
+    }
+}
---
+++ ConvertTemp/ConvertTemp/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConvertTemp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void konverteraKnapp_Click(object sender, EventArgs e)
+        {
+            //RequiredFieldValidator1.Visible = false;
+            //RequiredFieldValidator2.Visible = false;
+            //RequiredFieldValidator3.Visible = false;
+
+            if(IsValid)
+            {
+                //Rendewra ut tabellen här
+                //Med hjälp av klasserna TableRow och TableCell
+                //som läggs till i table-kontrollen
+
+                //Räkna ut vilka c/f fu ska skicka med till klassen.
+
+
+                for (int i = int.Parse(starttemp.Text); i <= int.Parse(sluttemp.Text); i = i + int.Parse(steg.Text))
+                {
+
+                    if (konvTyp1.Checked)
+                    {
+                        Model.TemperatureConverter.FahrenheitToCelcius(i);
+                    }
+
+                    else if (konvTyp1.Checked)
+                    {
+                        Model.TemperatureConverter.CelsiusToFahrenheit(i);
+                    }
+
+                    
+                }
+                
+            }
+        }
+    }
+}
